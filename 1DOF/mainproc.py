3dc9f4675faf6bd16e49a2578c6cfeded39b504a
Felipe Miotto <160151821@aluno.unb.br>
2022-06-24 16:22:30 -0300

    spaces

--- 1DOF/mainproc.py
+++ 1DOF/mainproc.py
@@ -5,13 +5,12 @@ from mecanica.sistema_pmma1 import sistema_pmma1 as pmma1
 
 # Código Principal ###########################################################
 
-
 def main():
     sistema = pmma1(79.2, 1000000, 0.0005)
     sistema.show()
 
-# Chamada em linha de comando do main code ###################################
 
+# Chamada em linha de comando do main code ###################################
 
 if __name__ == "__main__":
     main()
