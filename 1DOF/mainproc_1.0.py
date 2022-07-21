04e14d2c870ab9cda0dda1411493b01fa35b4a94
Felipe Miotto <160151821@aluno.unb.br>
2022-07-21 09:12:45 -0300

    pasta nova para HGM

--- 1DOF/mainproc_1.0.py
+++ 1DOF/mainproc_1.0.py
@@ -9,8 +9,11 @@ def main():
     sistema = pmma1(79.2, 1000000, 0.0005)
     sistema.show()
 
+    sistema2 = pmma1(15, 2500, 0.002)
+    sistema2.show()
 
 # Chamada em linha de comando do main code ###################################
+
 
 if __name__ == "__main__":
     main()
--- 1DOF/mecanica/resonances.py
+++
@@ -1,4 +0,0 @@
-class resonances_pmma1:
-    def resonances(self, interval):
-
-        f = lambda w: (-w**2*m1 + k1)
